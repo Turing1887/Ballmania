bc23f54d5ce03a07ca85547c7c7903ef36176f07
Turing1887 <tim.zschage@t-online.de>
2017-08-24 20:41:58 +0200

    appdäit

--- Ballmania 2017/Assets/Lobby/Scripts/Lobby/GameLobbyHook.cs
+++ Ballmania 2017/Assets/Lobby/Scripts/Lobby/GameLobbyHook.cs
@@ -7,16 +7,18 @@ using UnityEngine.Networking;
 namespace Prototype.NetworkLobby
 {
     public class GameLobbyHook : LobbyHook {
-
+		public HealthUI healthUI;
         public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
         {
             LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
             MovementManagerScript player = gamePlayer.GetComponent<MovementManagerScript>();
             player.playerName = lobby.playerName;
             player.color = lobby.playerColor;
+	
 
-			HealthUI healthUI = GameObject.Find ("HUDCanvas").GetComponent<HealthUI>();
-			healthUI.activePlayers.Add (player.playerName);
+//			HealthUI healthUI = GameObject.Find ("GameManager").GetComponent<HealthUI>();
+////			healthUI = NetworkServer.FindLocalObject();
+//			healthUI.activePlayers.Add (player.playerName);
 
 
         }
