2100442042ce9afe7df3ac8f6fb68d754c5b2d73
Turing1887 <tim.zschage@t-online.de>
2017-08-29 22:26:28 +0200

    finished simple menu | #39

---
+++ Ballmania 2017/Assets/LoadLobby.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LoadLobby : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Button> ().onClick.AddListener (Load);
+	}
+	void Load(){
+		SceneManager.LoadScene ("Lobby Scene");
+	}
+	
+
+}
---
+++ Ballmania 2017/Assets/QuitButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuitButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Button>().onClick.AddListener (QuitGame);
+	}
+	
+	void QuitGame(){
+		Application.Quit ();
+	}
+}
